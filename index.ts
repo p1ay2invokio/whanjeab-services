1f7d35a3150347f0a88df44ff41532cc8a5b8f22
p1ay2invokio <play21947@gmail.com>
2025-12-03 12:22:28 +0700

    fix/docker-2

--- index.ts
+++ index.ts
@@ -142,6 +142,6 @@ app.post('/push', async (req, res) => {
     }
 })
 
-app.listen(3002, () => {
-    console.log(`Server is running on port 3002`)
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
 })
